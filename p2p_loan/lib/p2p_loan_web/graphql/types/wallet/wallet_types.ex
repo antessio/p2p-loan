a6318f112d2f63cf29742fdc36c62702ae3f7a2c
antessio <antessio7@gmail.com>
2024-09-11 09:41:47 +0200

    movements in wallet schema

--- p2p_loan/lib/p2p_loan_web/graphql/types/wallet/wallet_types.ex
+++ p2p_loan/lib/p2p_loan_web/graphql/types/wallet/wallet_types.ex
@@ -7,6 +7,19 @@ defmodule P2pLoanWeb.GraphQL.Wallet.WalletTypes do
     field :amount, non_null(:decimal)
     field :owner_id, non_null(:string)
     field :currency, non_null(:string)
+    field :movements, list_of(:movement) do
+      resolve fn wallet, _args, _resolution ->
+        batch({__MODULE__, :list_movement_by_wallet_id, P2pLoan.Wallets.WalletMovement}, wallet.id, fn batch_results ->
+          {:ok, Map.get(batch_results, wallet.id)}
+        end)
+      end
+    end
+  end
+
+  def list_movement_by_wallet_id(_, wallet_ids) do
+    movements = P2pLoan.Wallets.list_movement_by_wallet_id(wallet_ids)
+    Enum.group_by(movements, fn movement -> movement.wallet_id end)
+
   end
 
   object :movement do
